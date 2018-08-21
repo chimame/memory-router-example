27a7a6784a5f53af98775effcc41d205de101ed6
chimame <rito.tamata@gmail.com>
2018-08-21 22:07:27 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { MemoryRouter, Route, Link } from 'react-router-dom'
+
+const App = () => (
+  <MemoryRouter>
+    <div>
+      <Route exact path='/' component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/friends' component={Friends} />
+    </div>
+  </MemoryRouter>
+)
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <p>Welcome to ようこそ</p>
+  </div>
+)
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>フレンズに投票するページです</p>
+  </div>
+)
+const Friends = () => (
+  <div>
+    <h2>Friends</h2>
+    <p>ここにフレンズのリストを書きます</p>
+  </div>
+)
+
+export default App
